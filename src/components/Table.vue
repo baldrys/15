6a448c18b338dbb1947657f076c0bd593c1fe2e3
mikhail <korzinadlyamusora@gmail.com>
2020-01-19 06:52:51 +0300

    Добавлена возможноть сдвигать сразу несклько ячеек

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -24,24 +24,61 @@ export default {
     name: 'Table',
     methods: {
       cellClicked(i, j){
-        const cellClicked = {x:i, y:j};
-        let cellToSwap = null;
-      if( i-1 >= 0 && this.cells[i-1][j] == ''){
-        cellToSwap = {x:i-1, y:j};
-      } else if (j+1 <= this.cells[0].length - 1 && this.cells[i][j+1] == '') {
-        cellToSwap = {x:i, y:j+1};
-      } else if (i+1 <= this.cells[0].length - 1 && this.cells[i+1][j] == '') {
-        cellToSwap = {x:i+1, y:j};
-      } else if (j-1 >=0 && this.cells[i][j-1] == '') {
-        cellToSwap = {x:i, y:j-1};
-      }
-      if(cellToSwap) {
-        // console.log(cellClicked);
-        // console.log(cellToSwap, cellClicked);
-        const payload = {cell1:cellToSwap, cell2:cellClicked};
-        this.$store.dispatch('TableStore/swapCells', payload)
+      for (let l = 0; l < this.cells[0].length; l++) {
+        if(this.cells[i][l] == ''){
+          // Right shift
+          if( j < l) {
+            for (let m = l-1; m >= j; m--) {
+                const payload = {
+                  cell1: {x:i, y:m+1},
+                  cell2: {x:i, y:m},
+                };
+                this.$store.dispatch('TableStore/swapCells', payload)
+            }
+          } else {
+            // Left shift
+            for (let m = l+1; m <= j; m++) {
+                const payload = {
+                  cell1: {x:i, y:m-1},
+                  cell2: {x:i, y:m},
+                };
+                this.$store.dispatch('TableStore/swapCells', payload)
+            }
+          }
+          break
+        }
+        if(this.cells[l][j] == ''){
+          // Down shift
+          if( i < l) {
+            for (let m = l-1; m >= i; m--) {
+                const payload = {
+                  cell1: {x:m+1, y:j},
+                  cell2: {x:m, y:j},
+                };
+                this.$store.dispatch('TableStore/swapCells', payload)
+            }
+          } else {
+            // Up shift
+            for (let m = l+1; m <= i; m++) {
+                const payload = {
+                  cell1: {x:m-1, y:j},
+                  cell2: {x:m, y:j},
+                };
+                this.$store.dispatch('TableStore/swapCells', payload)
+            }
+          }
+          break
+        } 
         
       }
+
+      // if(cellToSwap) {
+      //   // console.log(cellClicked);
+      //   // console.log(cellToSwap, cellClicked);
+      //   const payload = {cell1:cellToSwap, cell2:cellClicked};
+      //   this.$store.dispatch('TableStore/swapCells', payload)
+        
+      // }
 
       }
     },
